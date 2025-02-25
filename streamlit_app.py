bf2d7bffdc1f0767c874a96f5d4ab582723ad1a6
xlx21 <xlx21@mails.tsinghua.edu.cn>
2025-02-25 05:32:51 +0000

    qa

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import requests
 
 # FastAPI 服务的地址
-API_URL = "https://b3ba-43-247-185-76.ngrok-free.app/generate"
+API_URL = "https://f895-43-247-185-76.ngrok-free.app/generate"
 
 # 页面标题
-st.title("Vicuna-7B 对话演示")
+st.title("对话演示")
 
 # 用户输入
 user_input = st.text_input("请输入你的问题：")
@@ -15,9 +15,9 @@ if st.button("生成回复"):
     if user_input:
         # 调用 FastAPI 服务
         try:
-            response = requests.get(
+            response = requests.post(
                 API_URL,
-                params={"input_text": user_input}
+                json={"input_text": user_input}
             )
             if response.status_code == 200:
                 generated_text = response.json()["generated_text"]
